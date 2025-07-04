cfaa5d0b2811a0cd2feed3bfbbd9dacaf8a5d47b
lucasdwn <lfcosta0804@gmail.com>
2025-07-03 21:08:14 -0300

    update CORS

--- app/main.py
+++ app/main.py
@@ -1,6 +1,24 @@
 from fastapi import FastAPI
 from app.routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Configure aqui os domínios que podem acessar seu backend
+origins = [
+    "http://localhost:5173",  # endereço do seu frontend em dev
+    "http://127.0.0.1:5173",  # às vezes usa esse também
+    # Se quiser liberar geral (não recomendado para produção):
+    # "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(router)
